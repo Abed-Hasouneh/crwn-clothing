6cc4d4a6f26a298c932d2c1837ee50f39b196769
Abed-Hasouneh <a.r.hasouneh@gmail.com>
2022-04-18 01:46:51 +0300

    shop page

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import SHOP_DATA from '../../shop-data.json'
+
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      {
+        SHOP_DATA.map(({id,name})=>(
+          <div key={id}>
+            <h1>{name}</h1>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
