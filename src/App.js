dc999b09a40b4b9298ad29941d7f9ebb567197bc
Abed-Hasouneh <a.r.hasouneh@gmail.com>
2022-04-18 11:16:35 +0300

    checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./routes/Home/Home";
 import Navigation from "./routes/navigation/Navigation";
 import Shop from "./routes/shop/Shop";
 import Authentication from "./routes/authentication/Authentication";
+import Checkout from "./routes/checkout/Checkout";
 
 const App = () => {
   return(
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Home/>}/>
         <Route path="shop" element={<Shop/>}/>
         <Route path="auth" element={<Authentication/>}/>
+        <Route path="checkout" element={<Checkout/>} />
       </Route>
       
     </Routes>
