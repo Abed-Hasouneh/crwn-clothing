402d4bf50189bdd6ac7cf43be5ae8ea257c6df22
Abed-Hasouneh <a.r.hasouneh@gmail.com>
2022-04-19 13:12:36 +0300

    using categoriesMap

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -3,12 +3,12 @@ import { createContext, useEffect, useState } from 'react';
 import { getCategoriesAndDocuments  } from '../utils/firebase/firebase.utils';
 
 
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap:{},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   // useEffect(() => {
   //   addCollectionAndDocuments('collections', SHOP_DATA);
@@ -18,15 +18,16 @@ export const ProductsProvider = ({ children }) => {
     const getCategoriesMap = async()=>{
     const categoryMap = await getCategoriesAndDocuments();
     console.log(categoryMap);
+    setCategoriesMap(categoryMap)
     }
     getCategoriesMap()
   }, []);
 
-  const value = { products };
+  const value = { categoriesMap };
 
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };
