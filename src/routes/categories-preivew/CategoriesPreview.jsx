4260422a5326df18cf8aa0c8290c73f7374c50e5
Abed-Hasouneh <a.r.hasouneh@gmail.com>
2022-05-07 23:33:30 +0300

    sss

--- src/routes/categories-preivew/CategoriesPreview.jsx
+++ src/routes/categories-preivew/CategoriesPreview.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment} from 'react'
+import React from 'react'
 import CategoryPreview from '../../components/category-preview/CategoryPreview';
 
 import { useSelector } from 'react-redux';
